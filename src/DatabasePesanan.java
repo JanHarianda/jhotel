18885e365b070d669e4a07ec106ab6e58936d233
Jan Harianda Barus <jan.harianda@ui.ac.id>
2018-04-12 19:16:14 +0700

    tugas1

--- src/DatabasePesanan.java
+++ src/DatabasePesanan.java
@@ -24,27 +24,89 @@ public class DatabasePesanan
 
    public static boolean addPesanan(Pesanan baru)
    {
-       return false;
+       if (PESANAN_DATABASE.contains (baru))
+       {
+           if (baru.getStatusAktif())
+           {
+               return false;
+           }
+           else
+           {
+               PESANAN_DATABASE.add(baru);
+               return true;
+           }
+       }
+       else
+       {
+           PESANAN_DATABASE.add(baru);
+           return true;
+       }
     }
     
     public static Pesanan getPesanan(int id)
     {
+        for(Pesanan pesanan : PESANAN_DATABASE)
+        {
+            if(pesanan.getID() == id)
+            {
+                return pesanan;
+            }
+        }
         return null;
     }
     
    public static Pesanan getPesanan(Room kamar)
-   {       
+   {
+       for(Pesanan pesanan : PESANAN_DATABASE)
+       {
+           if(pesanan.getRoom().equals(kamar))
+           {
+               return pesanan;
+           }
+       }
        return null;
    }
 
    public static Pesanan getPesananAktif(Customer pelanggan)
    {
+       for(Pesanan pesanan : PESANAN_DATABASE)
+       {
+           if(pesanan.getPelanggan().equals(pelanggan))
+           {
+               if(pesanan.getStatusAktif())
+               {
+                   return pesanan;
+               }
+           }
+       }
        return null;
    }
     
 
     public static boolean removePesanan(Pesanan pesan)
     {
+        for(Pesanan pesanan : PESANAN_DATABASE)
+        {
+            if(pesanan.equals(pesan))
+            {
+                if(pesanan.getRoom() != null)
+                {
+                    Administrasi.pesananDibatalkan(pesanan);
+                }
+                else
+                {
+                    if(pesanan.getStatusAktif())
+                    {
+                        pesanan.setStatusAktif(false);
+                    }
+                }
+
+                if(PESANAN_DATABASE.remove(pesanan))
+                {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
--- src/DatabaseRoom.java
+++ src/DatabaseRoom.java
@@ -1,26 +1,89 @@
+import java.util.ArrayList;
+
 /**
- * Kelas ini berisi Database Room
+ * Kelas inii berisi Database Room
  * 
  * @author (Jan Harianda Barus)
  * @version (March 10, 2018)
  */
 public class DatabaseRoom
 {
-    private static String[] list_room;
+    private static ArrayList<Room> ROOM_DATABASE = new ArrayList<Room>();
 
-    public static boolean addRoom(Customer baru) 
+    /**
+     * Method yang digunakan untuk mengembalikan database kamar.
+     * @return ArrayList<Room> mengembalikan database kamar.
+     */
+
+    public static ArrayList<Room> getRoomDatabase()
     {
+        return ROOM_DATABASE;
+    }
+
+
+    public static boolean addRoom(Room baru)
+    {
+        for(Room kamar : ROOM_DATABASE)
+        {
+            if(!kamar.getHotel().equals(baru.getHotel()) &&
+                    !kamar.getNomorKamar().equals(baru.getNomorKamar()))
+            {
+                ROOM_DATABASE.add(baru);
+                return true;
+            }
+        }
+
         return false;
     }
-    
-    public static boolean removeRoom(int id) 
+
+    /**
+     * Method yang digunakan untuk mencari kamar dari database.
+     * @param hotel, nomor_kamar input customer yang akan dimasukkan kedalam database
+     * @return Room mengembalikan kamar.
+     */
+
+    public static Room getRoom(Hotel hotel, String nomor_kamar)
     {
-        return false;
-    }
-    
-    public static String[] getRoomDatabase() 
-    {
+        for(Room kamar : ROOM_DATABASE)
+        {
+            if(kamar.getHotel().equals(hotel) &&
+                    kamar.getNomorKamar().equals(nomor_kamar))
+            {
+                return kamar;
+            }
+        }
+
+
+        public static ArrayList<Room> getRoomsFromHotel(Hotel hotel)
+        {
+            ArrayList<Room> tempRoom = new ArrayList<Room>();
+
+            for(Room kamar : ROOM_DATABASE)
+            {
+                if(kamar.getHotel().equals(hotel))
+                {
+                    tempRoom.add(kamar);
+                }
+            }
+
+            return null;
+        }
+
         return null;
     }
-    
-}
+    public static boolean removeRoom(Hotel hotel, String nomor_kamar)
+    {
+        for(Room kamar : ROOM_DATABASE)
+        {
+            if(kamar.getHotel().equals(hotel) &&
+                    kamar.getNomorKamar().equals(nomor_kamar))
+            {
+                Administrasi.pesananDibatalkan(kamar);
+                if(ROOM_DATABASE.remove(kamar))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }}
